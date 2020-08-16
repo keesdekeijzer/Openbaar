04842a8ac248eb00bca9cae9afca0e0bd58804c2
Kees de Keijzer <47939634+keesdekeijzer@users.noreply.github.com>
2020-08-16 10:12:11 +0200

    Add files via upload

--- neon-post-install.sh
+++ neon-post-install.sh
@@ -7,20 +7,39 @@ apt install bluefish -y
 apt install calibre -y
 apt install chkrootkit -y
 apt install clamav -y
-apt install eclipse -y
 apt install ffmpeg -y
 apt install filezilla -y
 apt install gimp -y
+apt install git -y
+apt install gitg -y
 apt install gpodder -y
 apt install gscan2pdf -y
 apt install htop -y
 apt install inkscape -y
+apt install k3b -y
+apt install kate -y
+apt install kcolorchooser -y
+apt install kdenlive -y
+apt install kdevelop -y
+apt install krdc -y
 apt install libreoffice -y
 apt install pandoc -y
 apt install php -y
 apt install postgresql -y
+apt install qtcreator -y
 apt install rsync -y
 apt install scribus -y
+apt install sigil -y
+apt install synaptic -y
+apt install syncthing -y
 apt install tesseract-ocr -y
+apt install thonny -y
 apt install tmux -y
+apt install zim -y
+
+snap install teams-for-linux
+snap install --classic atom
+snap install --classic code
+snap install --classic pycharm-community
+
 
