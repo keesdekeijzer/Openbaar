8df77283cf6f6f31782132ff0e5eea8a8b74b3df
Kees de Keijzer <47939634+keesdekeijzer@users.noreply.github.com>
2020-08-16 07:26:49 +0200

    Add files via upload

---
+++ neon-post-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# script om een nieuwe installatie van Neon aan te vullen met applicaties
+
+apt install apache2
+apt install audacity
+apt install bluefish
+apt install calibre
+apt install chkrootkit
+apt install clamav
+apt install eclipse
+apt install ffmpeg
+apt install filezilla
+apt install gimp
+apt install gpodder
+apt install gscan2pdf
+apt install htop
+apt install inkscape
+apt install libreoffice
+apt install mysql-client
+apt install mysql-server
+apt install pandoc
+apt install php
+apt install postgresql
+apt install rsync
+apt install samba
+apt install scribus
+apt install tesseract-ocr
+apt install ufw
+apt install vim
+apt install vlc
+
